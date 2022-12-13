18fd36d59f8e872754e4e8cf8593ef0cd31763d3
RabjamX2 <rabjam123@gmail.com>
2022-12-13 18:01:03 -0500

    commit message

---
+++ README.md
@@ -0,0 +1,45 @@
+theChattyOne - a python based Discord bot
+========================
+
+Before you start
+----------
+
+This project has a virtual environment made using the built in pyhton module `venv` via `python -m venv venv`
+
+You can activate the virtual environment with 
+
+```bash
+venv\Scripts\activate.bat
+```
+
+The virtual environment has the modules `discord` and `openai` installed via `pip`
+
+**If you don't want to mess with the venv or don't mind installing globally**, You can just run `pip install discord` and `pip install openai` and have no need for the `venv` folder
+
+Setting up
+--------
+
+The only setup needed as of now is to insert your token and channelID into the *config.py.RENAMEME* file and rename it *config.py*
+
+Your [OpenAI Token](https://beta.openai.com/account/api-keys), discord bot token, and Discord channel ID is needed.
+You can find the ID of a discord channel by enabling developer mode in the Discord `Settings>App Settings>Advanced>Developer Mode` and the right clicking a channel
+
+**To start the bot**, When the virtual environment is activated, you can run `bot.py` to activate the bot
+
+```python
+python bot.js
+```
+You can use either Ctrl^C to exit or the coded *client.close()* function (type bye in the designated channel)
+
+Running
+--------
+
+
+Useful links
+-----------------
+
+[Discord Intro to Bots and Apps](https://discord.com/developers/docs/intro#bots-and-apps)
+
+[OpenAI API](https://beta.openai.com/docs/api-reference/introduction)
+
+[Discord Python API](https://discordpy.readthedocs.io/en/stable/quickstart.html)
---
+++ bot.py
@@ -0,0 +1,73 @@
+import openai
+import discord
+from config import openAIToken, discordBotToken, channelID
+
+# Set up the OpenAI API key
+openai.api_key = openAIToken
+
+# Create a new Discord client
+intents = discord.Intents.default()
+intents.members = True
+intents.message_content = True
+client = discord.Client(intents=intents)
+
+# Define the target Discord channel
+channel_id = channelID
+
+# Triggers
+imageTrigger = "image of "
+closeTrigger = "bye"
+
+# Handle messages received in the target Discord channel
+@client.event
+async def on_message(message):
+    # To ignore itself
+    if message.author == client.user:
+        return
+
+    # Stop command
+    if message.content == closeTrigger:
+        await client.close()
+
+    # Check if the message content is flagged by the moderation model
+    flagged = openai.Moderation.create(
+        input=message.content
+    ).results[0]
+
+    # If the message is flagged, do not generate a response
+    if flagged["flagged"]:
+        for flag in flagged["categories"]:
+            if flagged["categories"][flag]:
+                await message.channel.send(f"Can't reply since you have been flagged for '''{flag}'''")
+        return
+
+    if message.content.startswith(imageTrigger):
+        response = openai.Image.create(
+            prompt=message.content[len(imageTrigger):],
+            n=1,
+            size="1024x1024"
+        ).data[0].url
+        
+        await message.channel.send(response)
+
+    if message.channel.id == channel_id:
+        # Filters out imageTriggers
+        if message.content.startswith(imageTrigger):
+            return
+        else:
+            # Generate a response using the chatGPT model
+            response = openai.Completion.create(
+                engine="text-davinci-003",
+                prompt=message.content,
+                max_tokens=1024,
+                n=1,
+                #Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
+                temperature=0.9
+            ).choices[0].text
+
+            # Send the response to the Discord channel
+            await message.channel.send(response)
+
+
+# Run the Discord client
+client.run(discordBotToken)
